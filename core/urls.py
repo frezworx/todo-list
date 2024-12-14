4c61cec30d4804c617ccea1a4e79fd4de03a85fb
Frezworx <frezworx@gmail.com>
2024-12-14 15:29:14 +0200

    Add task and tag views, URLs, and templates integration
    Introduced generic views for task and tag management, including list, create, update, delete, and toggle functionality. Integrated these views into the URL configuration and connected the project to the template's directory. This sets up the foundation for managing tasks and tags within the application.

---
+++ core/urls.py
@@ -0,0 +1,32 @@
+from django.urls import path
+
+from core.views import (
+    TaskListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    TaskToggleView,
+    TagListView,
+    TagsCreateView,
+    TagsUpdateView,
+    TagsDeleteView,
+)
+
+app_name = "core"
+
+urlpatterns = [
+    path("task_list/", TaskListView.as_view(), name="task-list"),
+    path("task_create/", TaskCreateView.as_view(), name="task-create"),
+    path("task/<int:pk>/update", TaskUpdateView.as_view(), name="task-update"),
+    path("task/<int:pk>/delete", TaskDeleteView.as_view, name="task-delete"),
+    path("task/<int:pk>/toggle", TaskToggleView.as_view, name="task-toggle"),
+    path("tags/", TagListView.as_view(), name="tags-list"),
+    path("tags/create/", TagsCreateView.as_view(), name="tags-create"),
+    path("tags/<int:pk>update", TagsUpdateView.as_view(), name="tags-update"),
+    path(
+        "tags/<int:pk>/delete/",
+        TagsDeleteView.as_view(),
+        name="tags-delete"
+    ),
+
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class TaskListView(generic.ListView):
+    pass
+
+
+class TaskCreateView(generic.CreateView):
+    pass
+
+
+class TaskUpdateView(generic.UpdateView):
+    pass
+
+
+class TaskDeleteView(generic.DeleteView):
+    pass
+
+
+class TaskToggleView(generic.View):
+    pass
+
+
+class TagListView(generic.ListView):
+    pass
+
+
+class TagsCreateView(generic.CreateView):
+    pass
+
+
+class TagsUpdateView(generic.UpdateView):
+    pass
+
+
+class TagsDeleteView(generic.DeleteView):
+    pass
